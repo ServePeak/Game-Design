48a13aed297d5da395e70063fcb0ca44b70f9fee
Jia <jiawu17@gmail.com>
2018-01-28 18:45:29 -0500

    Initial Prototype 0 commit

---
+++ Prototype 0/Assets/BubbleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+
+
+	// Use this for initialization
+	void Start () {
+		float moveHorizontal = Random.Range(1.02f,2.02f);
+		float moveVertical = Random.Range(1.02f,2.02f);
+
+
+		if (Random.Range (0.0f, 1.0f) > 0.5f) {
+			moveHorizontal = -moveHorizontal;
+		}
+		if (Random.Range (0.0f, 1.0f) > 0.5f) {
+			moveVertical = -moveVertical;
+		}
+
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		rb2d = GetComponent<Rigidbody2D> ();
+
+		rb2d.AddForce (movement, ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+}
---
+++ Prototype 0/Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public GameObject[] inside;
+
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate() {
+		bool inside_obj = false;
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement);
+//		Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+//		Debug.Log (mousePosition);
+//		rb2d.MovePosition(mousePosition);
+
+		foreach (GameObject obj in inside) {
+			if (this.transform.position.x < obj.transform.position.x + 0.7 & this.transform.position.x > obj.transform.position.x - 0.7 &
+				this.transform.position.y < obj.transform.position.y + 0.7 & this.transform.position.y > obj.transform.position.y - 0.7) {
+				inside_obj = true;
+				break;
+			}
+		}
+		if (inside_obj == false) {
+			Application.LoadLevel (0);  
+		}
+	}
+}
