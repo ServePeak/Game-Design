3d5235558ce9b595c649ba38179aeb2c9b453c53
Jia Wu <ratedrion@gmail.com>
2018-03-26 17:28:11 -0400

    Stage 4 Prototype 6

--- Prototype 6/Assets/PlayerMove.cs
+++ Prototype 6/Assets/PlayerMove.cs
@@ -15,6 +15,12 @@ public class PlayerMove : MonoBehaviour {
     private bool play_3;
     private bool play_4;
     private Rigidbody2D rb;
+    public bool follow;
+    public GameObject spook;
+    public GameObject foot;
+    private float timer;
+    private List<GameObject> footsteps = new List<GameObject>();
+    private GameObject spawned;
 
     // Use this for initialization
     void Start () {
@@ -23,11 +29,21 @@ public class PlayerMove : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
+        timer += Time.deltaTime;
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector2(moveHorizontal, moveVertical);
+
+        if (timer > 0.2f) {
+            if (movement.x > 0 || movement.y > 0) {
+                spawned = Instantiate(foot, new Vector3(transform.position.x, transform.position.y, 0f), Quaternion.identity);
+                footsteps.Add(spawned);
+
+                timer = 0;
+            }
+        }
 
         rb.velocity = movement * speed;
 
@@ -37,12 +53,17 @@ public class PlayerMove : MonoBehaviour {
         } else if (transform.position.x > 20.0f && transform.position.y > 10.0f && !play_2) {
             audioSource.PlayOneShot(sound2, 0.5F);
             play_2 = true;
+            follow = true;
         } else if (transform.position.x < -10.0f && transform.position.y < 5.0f && !play_3) {
             audioSource.PlayOneShot(sound3, 0.5F);
             play_3 = true;
         } else if (transform.position.x > 30.0f && transform.position.y > 15.0f && !play_4) {
             audioSource.PlayOneShot(sound4, 0.5F);
             play_4 = true;
+        }
+
+        if (follow) {
+            spook.gameObject.GetComponent<EnemyMove>().enabled = true;
         }
     }
 }
